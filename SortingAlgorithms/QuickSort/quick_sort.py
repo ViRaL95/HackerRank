8cd77c329d6584ef4625b16eea58ff1ef52681d3
Varun <vrao@gogoair.com>
2017-11-09 10:01:08 -0500

    Quick Sort, Merg Sort, remove duplicates from linked list

--- SortingAlgorithms/QuickSort/quick_sort.py
+++ SortingAlgorithms/QuickSort/quick_sort.py
@@ -8,3 +8,41 @@
 
 # In order to sort each sublist we have to use the partition logic again. We pick a random pivot from the sublist and we rearrange teh sublist such that all the elemnets that are less are to teh less. 
 
+#The quick sort algorithm uses divide and conquer to gain the same advantages as merge sort (n logn) but without additional storage. When the list is not able to be divded by half then the performance diminishes. 
+
+
+def QuickSort(A, start, end):
+    if (start >= end):
+        return 
+    partition_index = Partition(A, start, end)
+    #divide and conquer methodology
+    QuickSort(A, start, partition_index -1 )
+    QuickSort(A, partition_index + 1, end)
+
+
+def Partition(A, start, end):
+    #The partition index is essentially used to reprsent the partition
+    #Elements in the array are iterated through and compare against the
+    #pivot value if they are smaller then the value at the partitoin index 
+    #and the element in the array we are currently at within the iteration are 
+    #swapped. Finally because the partition index is just a representation of 
+    # the actual partition we swap the two variables
+    pivot = A[end]
+    partition_index = start
+    for i in range(start, end):
+        if A[i] <= pivot and partition_index != i:
+            temporary = A[i]
+            A[i] = A[partition_index]
+            A[partition_index] = temporary
+            partition_index += 1
+    temporary = A[partition_index]
+    A[partition_index] = A[end]
+    A[end] = temporary
+    return partition_index
+
+
+if __name__ == '__main__':
+    A = [7,6,6,5,4,3,2,2,1,0,0]
+    QuickSort(A, 0, len(A) - 1)
+    for value in A:
+        print (value)
