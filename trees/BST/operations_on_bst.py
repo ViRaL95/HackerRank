7715e33d6b6e99cad05c78c6b58e78fed32d704a
Varun <vrao@gogoair.com>
2017-11-19 09:11:28 -0500

    Create Linked List for nodes at each depth in BST

--- trees/BST/operations_on_bst.py
+++ trees/BST/operations_on_bst.py
@@ -1,3 +1,8 @@
+class Link_Node:
+        def __init__(self, data=None, next_node=None):
+            self.data = data
+            self.next_node = next_node
+
 class Node:
     def __init__(self, left=None, right=None, data=None):
         self.left = left
@@ -73,11 +78,19 @@ class Node:
             return 1 + self.right.find_depth(data)
 
     def find_max(self):
+        """It is very important to know WHEN to use return and when not to use return. 
+        In this situation if I do not have an else condition and I simply have no return
+        statement under the if condition then this function will only return the root node.
+        When a function1 calls itself lets label it function2 recursively and the information returned from function2 needs to be given from function1 to its parent immediately then you are generally
+        using return in the correct situation. If for example I remove the return statemnet 
+        under the if condition the line of code underneath that line will execute each and every
+        time and then return to its parent
+        """
         if self.right:
-            return self.right.find_max()
+            self.right.find_max()
         return self.data
 
-
+    
     def find_min(self):
         if self.left:
             return self.left.find_min()
@@ -102,7 +115,39 @@ class Node:
             return self.left.lowest_common_ancestor(data1, data2)
         elif data1 > self.data and data2 > self.data:
             return self.right.lowest_common_ancestor(data1, data2)
+
+    
+    def create_linked_lists(self, depth, hash_map):
+        """This method returns n linked lists for all the nodes at each depth
         
+        DEPTH OF NODE - Number of edges from the node to the tree's root node
+
+        HEIGHT OF NODE- Number of edges on the longest parth from thee node to a leaf
+        """
+        if depth not in hash_map:
+            hash_map[depth] = Link_Node(self.data)
+        else:
+            head = hash_map[depth]
+            while (head.next_node):
+                head = head.next_node
+            head.next_node = Link_Node(self.data)
+        if self.left:
+            self.left.create_linked_lists(depth + 1, hash_map)
+        if self.right:
+            self.right.create_linked_lists(depth + 1, hash_map)
+
+    def check_if_in_hash_map(self, hash_map, depth, node):
+        if depth not in hash_map:
+            print("start")
+            print("depth %d" %depth)
+            hash_map[depth] = node
+        else:
+            print("yeah")
+            head = hash_map[depth]
+            while (head.next_node):
+                head = head.next_node
+            head.next_node = node
+
 class Tree:
     def __init__(self, root=None):
         self.root = root
@@ -142,6 +187,9 @@ class Tree:
     def lowest_common_ancestor(self, data1, data2):
         return self.root.lowest_common_ancestor(data1, data2)
 
+    def create_linked_list_each_depth(self, hash_map):
+        return self.root.create_linked_lists( 0, hash_map)
+
 if __name__ == '__main__':
     tree = Tree()
     tree.insert(5)
@@ -152,12 +200,19 @@ if __name__ == '__main__':
     tree.insert(4)
     tree.insert(2)
     tree.insert(3)
-    tree.pre_order_traversal()
-    print("height of tree is %d"%tree.find_height())
-    print("depth of node 8 is %d"%tree.find_depth(8))
+    #tree.pre_order_traversal()
+    #print("height of tree is %d"%tree.find_height())
+    #print("depth of node 8 is %d"%tree.find_depth(8))
     print(tree.find_max())
-    print(tree.find_min())
-    print("left view of tree")
-    tree.left_view()
-    print(tree.lowest_common_ancestor(4, 6))
+    #print(tree.find_min())
+    #print("left view of tree")
+    #tree.left_view()
+    #print(tree.lowest_common_ancestor(4, 6))
+    hash_map = {}
+    tree.create_linked_list_each_depth(hash_map)
+    for key, value in hash_map.items():
+        print("depth %d"%key)
+        while (value):
+            print(value.data)
+            value = value.next_node
 
