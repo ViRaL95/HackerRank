9eb4e34c1da02839ea7d2282fa3302e55d674ec9
Varun <vrao@gogoair.com>
2018-01-17 13:35:16 -0500

    Remove dupicates using hash map and fixed original constant space solution

--- linkedlist/remove_duplicates_unsorted.py
+++ linkedlist/remove_duplicates_unsorted.py
@@ -1,26 +1,45 @@
-# 1 6 2 6 6 5 4 
-#1 6 2 
 class Node:
     def __init__(self, data=None, next_node=None):
         self.data = data
         self.next_node = next_node
 
-def remove_duplicates(head):
-    if not head or not head.next_node:
+def remove_duplicates_two(head):
+    if not head:
         return head
-    first_pointer = head
-    while(first_pointer.next_node):
-        prev = first_pointer
-        current = prev.next_node
-        while(current):
-            if current.data == first_pointer.data:
-                current = current.next_node
-            else:
-                prev.next_node = current
+    current = head
+    while current.next_node:
+        prev = current
+        third = current.next_node
+        while third:
+            if third.data != current.data:
+                prev.next_node = third
                 prev = prev.next_node
-                current = current.next_node
-        first_pointer = first_pointer.next_node
+            third = third.next_node
+        current = current.next_node
+        prev.next_node = None
     return head
+
+
+def remove_duplicates_hashmap(head):
+    if not head:
+        return head
+    hash_map = set()
+    prev = head
+    current = head.next_node
+    while(current):
+        if current.data not in hash_map:
+            hash_map.add(current.data)
+            prev.next_node = current
+            prev = prev.next_node
+        current = current.next_node
+    prev.next_node = None
+
+def print_nodes(head):
+    print("--------------------- PRINT NODES ---------------------")
+    temp = head
+    while temp:
+        print (temp.data)
+        temp = temp.next_node
 
 if __name__ == "__main__":
     start_element = 1
@@ -36,12 +55,10 @@ if __name__ == "__main__":
             start_node = start_node.next_node
             start_element += 1
         finish_element += 1
-
-    while(hi):
-        print(hi.data)
-        hi = hi.next_node
-    print("----------------------")
-    remove_duplicates(head)
-    while(head):
-        print(head.data)
-        head = head.next_node
+    start_node.next = Node(5)
+    start_node.next.next = Node(5)
+    print_nodes(head)
+    #remove_duplicates_two(head)
+    remove_duplicates_hashmap(head)
+    print_nodes(head)
+    
