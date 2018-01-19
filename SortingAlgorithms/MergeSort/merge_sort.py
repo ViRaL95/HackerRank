4960509a896a46c98ed521f84e03004585edc7a9
Varun <vrao@gogoair.com>
2018-01-18 19:14:53 -0500

    Feature: Created a More pythonic way of  performing merge sort(splice array rather than manually creating left and right array)

--- SortingAlgorithms/MergeSort/merge_sort.py
+++ SortingAlgorithms/MergeSort/merge_sort.py
@@ -6,46 +6,45 @@ There are two types of reference localities -- temporatl and spatial locality. T
 
 """
 def merge_sort(array, low, high):
-    #base case two elements
-    if high > low:
-        mid = low + int((high - low)/2) # (high - low)/2 = distance from center to low. low + center = actual index
-        merge_sort(array, low, mid) #including mid
-        merge_sort(array, mid + 1, high) #everything besides mid 
-        merge(array, low, mid, high) 
+    if low >= high:
+        return
+    mid = low + int((high - low)/2)
+    merge_sort(array, low, mid)
+    merge_sort(array, mid + 1, high)
+    merge(low, mid, high, array)
 
+def merge(low, mid, high, array):
+    #We tak the left and right array and then we create a new array
+    left_array = array[low: mid + 1]
+    right_array = array[mid + 1: high + 1]
 
-def merge(array, low, mid, right):
-    length_left_array = mid - low + 1#length_left_array is 1. You include the + 1 because youre including mid
-    length_right_array = right - mid #length right array is 1
-    left_array = [0] * length_left_array
-    right_array = [0] * length_right_array
-    
-    for element in range(length_left_array):
-        left_array[element] = array[low + element] #includes mid
-    for element in range(length_right_array):
-        right_array[element] = array[mid + element + 1] #doesnt include mid
-    index_left = 0
-    index_right = 0
+    right_index = 0
+    left_index = 0
     array_index = low
-    while (index_left < length_left_array and index_right < length_right_array):
-        if (left_array[index_left] <= right_array[index_right]):
-            array[array_index] = left_array[index_left]
-            index_left += 1
+    while  left_index < len(left_array) and right_index < len(right_array):
+        if left_array[left_index] < right_array[right_index]:
+            array[array_index] = left_array[left_index]
+            left_index += 1
+
         else:
-            array[array_index] = right_array[index_right]
-            index_right += 1
+            array[array_index] = right_array[right_index]
+            right_index += 1
+
         array_index += 1
-    while(index_left < length_left_array):
-        array[array_index] = left_array[index_left]
-        index_left += 1
+
+    while left_index < len(left_array):
+        array[array_index] = left_array[left_index]
+        left_index += 1
         array_index += 1
-    while(index_right < length_right_array):
-        array[array_index] = right_array[index_right]
-        index_right += 1
+
+    while right_index < len(right_array):
+        array[array_index] = right_array[right_index]
+        right_index += 1
         array_index += 1
-    return    
+    return
 
 if __name__ == '__main__':
     array = [32,1,9,6,2,19,0,-4,58]
+    #merge_sort(array, 0, len(array) - 1)
     merge_sort(array, 0, len(array) - 1)
     print(array)
